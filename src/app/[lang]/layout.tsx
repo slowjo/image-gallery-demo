a474da990fa5e9aa98b7ad5908bbe7dddc2f5a86
slowjo <fabiansen.s83@gmail.com>
2025-06-13 14:52:12 +0300

    add robots.txt file

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         {children}
