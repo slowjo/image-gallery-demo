eb844152773743d24c60a6713f5c0292a72ded7f
slowjo <fabiansen.s83@gmail.com>
2025-06-18 10:49:29 +0300

    change time to revalidat cache

--- src/app/[lang]/[rover]/actions.ts
+++ src/app/[lang]/[rover]/actions.ts
@@ -2,8 +2,8 @@
 
 export const getImages = async (rover : string, page : number) => {
     try {
-        const res = await fetch(`https://mars-photos.herokuapp.com/api/v1/rovers/${rover}/latest_photos?page=${page}`, { next: { revalidate: 30 } });
-        // const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=DEMO_KEY&page=${page}`, { next: { revalidate: 30 } });
+        // const res = await fetch(`https://mars-photos.herokuapp.com/api/v1/rovers/${rover}/latest_photos?page=${page}`, { next: { revalidate: 30 } });
+        const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=DEMO_KEY&page=${page}`, { next: { revalidate: 3600 } });
         // console.log('data: ', data);
         if (!res.ok) {
             throw new Error('fetch failed');
--- src/app/[lang]/[rover]/page.tsx
+++ src/app/[lang]/[rover]/page.tsx
@@ -22,8 +22,8 @@ export default async function DynamicRoverPage({
 
     const dict = await getDictionary(lang);
 
-    const res = await fetch(`https://mars-photos.herokuapp.com/api/v1/rovers/${rover}/latest_photos?page=1`, { next: { revalidate: 30 } });
-    // const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=DEMO_KEY&page=1`, { next: { revalidate: 30 } });
+    // const res = await fetch(`https://mars-photos.herokuapp.com/api/v1/rovers/${rover}/latest_photos?page=1`, { next: { revalidate: 30 } });
+    const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=DEMO_KEY&page=1`, { next: { revalidate: 3600 } });
     // const res2 = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/spirit?api_key=DEMO_KEY`, { next: { revalidate: 30 } });
     const data = await res.json();
     console.log('data: ', data);
