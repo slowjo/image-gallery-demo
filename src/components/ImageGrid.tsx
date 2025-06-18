b8e60fea745b959328e029a03f217f5b7535b91c
slowjo <fabiansen.s83@gmail.com>
2025-06-18 21:39:24 +0300

    add loading spinner

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -22,6 +22,7 @@ export default function ImageGrid({ latestPhotos, buttonLabel, fullPageViewButto
     const [page, setPage] = useState(2);
     const [photos, setPhotos] = useState(latestPhotos);
     const [maybeNoMorePhotos, setMaybeNoMorePhotos] = useState(false);
+    const [loading, setLoading] = useState(false);
     const observer = useRef<IntersectionObserver | null>(null);
 
     const lastImage = useCallback((node : Element) => {
@@ -36,6 +37,7 @@ export default function ImageGrid({ latestPhotos, buttonLabel, fullPageViewButto
             if (entries[0].isIntersecting) {
                 console.log('intersecting');
                 try {
+                    setLoading(true);
                     const moreImages = await getImages(rover, page);
                     if (!moreImages || !moreImages.length) {
                         setMaybeNoMorePhotos(true);
@@ -50,6 +52,8 @@ export default function ImageGrid({ latestPhotos, buttonLabel, fullPageViewButto
                 } catch {
                     setMaybeNoMorePhotos(true);
                     console.log('fetch failed on the client');
+                } finally {
+                    setLoading(false);
                 }
             }
         }, { threshold: 1.0 });
@@ -82,6 +86,17 @@ export default function ImageGrid({ latestPhotos, buttonLabel, fullPageViewButto
                 </div>    
             ))}
             </ul>
+            {
+                loading ? (
+                    <div className="loading-spinner">
+                        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
+                        </svg>
+                    </div>
+                ) : (
+                    null
+                )
+            }
             <div className={`imagemodal ${showImage ? '' : 'hidden'}`}>
                 {
                     selectedImage ? (
