3b32b5e9522b90655326a02307135db7bbb4590f
slowjo <fabiansen.s83@gmail.com>
2025-06-19 19:08:50 +0300

    fix loadingsquare props

--- src/components/LoadingGrid.tsx
+++ src/components/LoadingGrid.tsx
@@ -6,7 +6,8 @@ export default function LoadingGrid() {
             <ul className="imagegrid">
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((item) => (
                     <li key={item}>
-                        <LoadingSquare loading={true} />
+                        <LoadingSquare />
+                        {/* <LoadingSquare loading={true} /> */}
                     </li>
                 ))}
             </ul>
