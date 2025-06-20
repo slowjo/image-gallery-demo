af4e3070979f2b54f0ddbcbe9293e29674eb812c
slowjo <fabiansen.s83@gmail.com>
2025-06-20 11:16:56 +0300

    first fetch only

--- src/app/[lang]/[rover]/page.tsx
+++ src/app/[lang]/[rover]/page.tsx
@@ -1,6 +1,6 @@
-import { getDictionary } from "@/app/[lang]/dictionaries";
+// import { getDictionary } from "@/app/[lang]/dictionaries";
 // import ImageGrid from "@/components/ImageGrid";
-import { notFound } from "next/navigation";
+// import { notFound } from "next/navigation";
 import { rovers } from "@/data/rovers";
 
 export async function generateStaticParams() {
@@ -9,23 +9,26 @@ export async function generateStaticParams() {
   }))
 }
 
-export default async function DynamicRoverPage({
-  params,
-  searchParams,
-}: {
-  params: Promise<{ lang: string, rover: string }>,
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}) {
-    const { lang, rover } = await params;
-    const { sol : solParam } = await searchParams;
+export default async function DynamicRoverPage(
+//   {
+//   params,
+//   searchParams,
+// }: {
+//   params: Promise<{ lang: string, rover: string }>,
+//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+// }
+) {
+    // const { lang, rover } = await params;
+    // const { sol : solParam } = await searchParams;
 
-    if (!rovers.includes(rover)) {
-        notFound();
-    }
+    // if (!rovers.includes(rover)) {
+    //     notFound();
+    // }
 
-    const dict = await getDictionary(lang);
+    // const dict = await getDictionary(lang);
     
-    const res2 = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=DEMO_KEY`, { next: { revalidate: 3600 } });
+    const res2 = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/perseverance?api_key=z4pAcjV0pDhteos1MjvQy0oj1bt2gsSDfFA1Vn8E`, { next: { revalidate: 3600 } });
+    // const res2 = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=DEMO_KEY`, { next: { revalidate: 3600 } });
     
     if (!res2.ok) {
       return (
@@ -51,7 +54,7 @@ export default async function DynamicRoverPage({
 
     // const data3 = await res3.json();
     // console.log(solParam, data3, sol, dict, sols);
-    console.log(solParam, dict);
+    // console.log(solParam, dict);
       
     return (
       <div>
